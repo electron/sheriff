023902e652f8c3a5f4ab6d550b240b45b4d30f62
Samuel Attard <sam@electronjs.org>
2022-12-11 13:34:18 -0800

    refactor: use authNarrowing from auth-github-app (#26)
    This ensures that during a dry run we don't have a token with write access
    to anything in order to avoid any potential mishaps.

--- src/octokit.ts
+++ src/octokit.ts
@@ -2,12 +2,38 @@ import { graphql } from '@octokit/graphql';
 import { Octokit } from '@octokit/rest';
 import {
   appCredentialsFromString,
+  AuthNarrowing,
   getAuthOptionsForRepo,
   getTokenForRepo,
 } from '@electron/github-app-auth';
 import { SHERIFF_GITHUB_APP_CREDS, ORGANIZATION_NAME, REPO_NAME } from './constants';
+import { IS_DRY_RUN } from './helpers';
 
 require('dotenv-safe').config();
+
+function getAuthNarrowing(): AuthNarrowing {
+  // In a dry run, ensure we only have read access to resources to avoid
+  // any mishaps
+  if (IS_DRY_RUN) {
+    return {
+      permissions: {
+        administration: 'read',
+        members: 'read',
+        contents: 'read',
+        metadata: 'read',
+      },
+    };
+  }
+  // Otherwise we should get a token with write access to admin/members
+  return {
+    permissions: {
+      administration: 'write',
+      members: 'write',
+      contents: 'read',
+      metadata: 'read',
+    },
+  };
+}
 
 let octokit: Octokit;
 export async function getOctokit() {
@@ -20,6 +46,7 @@ export async function getOctokit() {
       name: REPO_NAME,
     },
     creds,
+    getAuthNarrowing(),
   );
   octokit = new Octokit({ ...authOpts });
   return octokit;
@@ -33,6 +60,7 @@ export async function graphyOctokit() {
       name: REPO_NAME,
     },
     creds,
+    getAuthNarrowing(),
   );
   return graphql.defaults({
     headers: {
